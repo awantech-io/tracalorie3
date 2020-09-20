a2f9a57a0adaf782e9e6d04800f7d63ad44d9458
awantech-io <hello.wanlab@gmail.com>
2020-09-20 09:49:11 +0800

    back

--- app.js
+++ app.js
@@ -283,6 +283,10 @@ const App = ( function(){
 
         // Update item event
         document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
+
+        // back button event
+        document.querySelector(UISelectors.backBtn).addEventListener('click', UICtrl.clearEditState);
+        
     }
 
     // Add item submit
